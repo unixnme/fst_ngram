30c27632ccbb9aa8246104bf7f8b231ccbd9966e
Unix&Me <unixnme@gmail.com>
2019-09-24 23:46:58 -0700

    add python script

---
+++ convert_to_log10.py
@@ -0,0 +1,11 @@
+import sys
+import numpy as np
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as f, open(sys.argv[1] + '.conv', 'w') as g:
+        for line in f:
+            tokens = line.split()
+            tokens[-1] = str(np.log10(np.exp(float(tokens[-1]))))
+            g.write('\t'.join(tokens))
+            g.write('\n')
+

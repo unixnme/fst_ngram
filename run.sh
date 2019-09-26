30cdaf5ae8fad1f5f4d0b9bbd9a86dc462ee1f5f
Unix&Me <unixnme@gmail.com>
2019-09-26 06:43:42 -0700

    update run.sh to add ngramapply

--- run.sh
+++ run.sh
@@ -10,12 +10,21 @@ fstprint --save_isymbols=isyms.txt --save_osymbols=osyms.txt binary.fst text.fst
 python ../convert_to_log10.py text.fst
 
 # compile the text WFST into binary WFST
-fstcompile --isymbols=isyms.txt --osymbols=osyms.txt text.fst.conv binary.fst.conv
+fstcompile --isymbols=isyms.txt --osymbols=osyms.txt --keep_isymbols --keep_osymbols text.fst.conv binary.fst.conv
 
 # for easier visualization, plot WFST
 fstdraw --portrait --isymbols=isyms.txt --osymbols=osyms.txt binary.fst.conv fst.dot
 dot -Tpdf fst.dot > fst.pdf
 
+# convert string input to far
+echo $2 > input.txt
+farcompilestrings --generate_keys=1 -symbols=isyms.txt --keep_symbols input.txt input.far
+
+# apply ngram to the input and print the cost (-log10(prob))
+echo "ngram probability (-log10(p))"
+ngramapply binary.fst.conv input.far | farprintstrings --print_weight
+echo
+
 # run perplexity analysis for the given sentence
-echo -e $2 | farcompilestrings --generate_keys=1 -symbols=isyms.txt --keep_symbols | ngramperplexity --v=1 binary.fst -
+ngramperplexity --v=1 binary.fst input.far
 

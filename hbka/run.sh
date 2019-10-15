a81741fa608ef58a3155ae2c3be6de020df84dd4
Unix&Me <unixnme@gmail.com>
2019-10-14 17:46:31 -0700

    rmeps after det/min

--- hbka/run.sh
+++ hbka/run.sh
@@ -7,11 +7,11 @@ fstprint --save_isymbols=osyms.txt G.fst > /dev/null
 cat osyms.txt | awk '{print $1}' | grep -v '<.*>' > vocab.txt
 python ../create_lex.py vocab.txt > L.txt
 fstcompile --isymbols=ascii.syms --osymbols=osyms.txt --keep_isymbols --keep_osymbols L.txt | fstarcsort --sort_type=olabel - L.fst
-fstcompose L.fst G.fst | fstproject - | fstdeterminize - | fstminimize - | fstarcsort --sort_type=ilabel - LG.fst
+fstcompose L.fst G.fst | fstproject - | fstdeterminize - | fstminimize - | fstrmepsilon - | fstdeterminize - | fstminimize - | fstarcsort - LG.fst
 
-fstcompile --isymbols=ascii.syms --osymbols=ascii.syms --keep_isymbols --keep_osymbols T.txt | fstarcsort --sort_type=olabel - T.fst
-fstcompose T.fst LG.fst TLG.fst
-fstrmepslocal TLG.fst | fstarcsort --sort_type=ilabel - TLG.fst.rmeps
+#fstcompile --isymbols=ascii.syms --osymbols=ascii.syms --keep_isymbols --keep_osymbols T.txt | fstarcsort --sort_type=olabel - T.fst
+#fstcompose T.fst LG.fst TLG.fst
+#fstrmepslocal TLG.fst | fstarcsort --sort_type=ilabel - TLG.fst.rmeps
 
 #fstdraw --portrait G.fst | dot -Tpdf > G.pdf
 #fstdraw --portrait L.fst | dot -Tpdf > L.pdf

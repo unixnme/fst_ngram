5f77bfded03b094d3da849fb35987ae0d4e8ecbc
Unix&Me <unixnme@gmail.com>
2020-05-25 15:00:31 -0700

    preserve stoachasticity as much as possible

--- hbka/run.sh
+++ hbka/run.sh
@@ -2,28 +2,29 @@ set -e
 set -x
 
 lmplz_dir=$1
-echo "be an app" | python ../create_lex.py
+echo "be an app pa" | python ../create_lex.py
 echo "a b e n p" | python ../create_t.py
 
-$lmplz_dir/lmplz --order 3 --discount_fallback < ../corpus.txt > 3gram.arpa
+$lmplz_dir/lmplz --order 3 --discount_fallback -S 10%  < ../corpus.txt > 3gram.arpa
 ngramread --ARPA --symbols=lexicon.syms 3gram.arpa | fstarcsort --sort_type=ilabel - G.fst
 fstcompile --isymbols=tokens.syms --osymbols=lexicon.syms --keep_isymbols --keep_osymbols lexicon.txt | fstarcsort --sort_type=olabel - L.fst
 
 fstcompile --isymbols=tokens.syms --osymbols=tokens.syms --keep_isymbols --keep_osymbols tokens.txt tokens.fst
 fstcompile --isymbols=tokens.syms --osymbols=tokens.syms --keep_isymbols --keep_osymbols tokens_self.txt tokens_self.fst
-fstarcsort tokens.fst | fstphicompose 1 tokens_self.fst - | fstrmepsilon | fstdeterminize | fstminimize - T.fst
-fstarcsort L.fst | fstcompose T.fst - | fstrmepsilon | fstminimizeencoded - TL.fst
+fstarcsort tokens.fst | fstphicompose 1 tokens_self.fst - | fstrmepsilon | fstdeterminize | fstminimize - | fstarcsort --sort_type=olabel - T.fst
+fstarcsort L.fst | fsttablecompose T.fst - | fstrmepsilon | fstminimizeencoded - TL.fst
 
 echo "0 1" > relabel.txt
 fstrelabel --relabel_ipairs=relabel.txt G.fst G.fst
+
 fstprint TL.fst TL.txt
 echo -e "0\t0\t<phi>\t<phi>\n" >> TL.txt # to compose with G's backoff ilabel
 fstcompile --isymbols=tokens.syms --osymbols=lexicon.syms --keep_isymbols --keep_osymbols TL.txt TL.fst
 
-fstarcsort --sort_type=olabel TL.fst | fstcompose - G.fst | fstrmepsilon | fstdeterminize | fstminimize - TLG.fst
+fstarcsort --sort_type=olabel TL.fst | fsttablecompose - G.fst | fstdeterminizestar | fstminimizeencoded - TLG.fst
 
 echo "1 0" > relabel.txt
-fstrelabel --relabel_ipairs=relabel.txt TLG.fst TLG.fst
+fstrelabel --relabel_ipairs=relabel.txt TLG.fst | fstrmepslocal - TLG.fst
 
 fstdraw --portrait G.fst | dot -Tpdf > G.pdf
 fstdraw --portrait L.fst | dot -Tpdf > L.pdf

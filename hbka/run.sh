332fd1aaae717b516cf8ef39e7b130e18de86ea1
Unix&Me <unixnme@gmail.com>
2020-05-26 15:06:41 -0700

    run.sh for actual corpus

--- hbka/run.sh
+++ hbka/run.sh
@@ -2,11 +2,10 @@ set -e
 set -x
 
 lmplz_dir=$1
-echo "be an app pa" | python ../create_lex.py
-echo "a b e n p" | python ../create_t.py
+python ../create_lex.py < ~/Data/corpus/vocab_200k.txt
+echo "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z '" | python ../create_t.py
 
-$lmplz_dir/lmplz --order 3 --discount_fallback -S 10%  < ../corpus.txt > 3gram.arpa
-ngramread --ARPA --symbols=lexicon.syms 3gram.arpa | fstarcsort --sort_type=ilabel - G.fst
+sed 's/<UNK>/<unk>/g' ../3-gram.pruned.3e-7.arpa | ngramread --ARPA --symbols=lexicon.syms - | fstarcsort --sort_type=ilabel - G.fst
 fstcompile --isymbols=tokens.syms --osymbols=lexicon.syms --keep_isymbols --keep_osymbols lexicon.txt | fstarcsort --sort_type=olabel - L.fst
 
 fstcompile --isymbols=tokens.syms --osymbols=tokens.syms --keep_isymbols --keep_osymbols tokens.txt tokens.fst
@@ -26,10 +25,10 @@ fstarcsort --sort_type=olabel TL.fst | fsttablecompose - G.fst | fstdeterminizes
 echo "1 0" > relabel.txt
 fstrelabel --relabel_ipairs=relabel.txt TLG.fst | fstrmepslocal - TLG.fst
 
-fstdraw --portrait G.fst | dot -Tpdf > G.pdf
-fstdraw --portrait L.fst | dot -Tpdf > L.pdf
-fstdraw --portrait T.fst | dot -Tpdf > T.pdf
-fstdraw --portrait TL.fst | dot -Tpdf > TL.pdf
-fstdraw --portrait TLG.fst | dot -Tpdf > TLG.pdf
+#fstdraw --portrait G.fst | dot -Tpdf > G.pdf
+#fstdraw --portrait L.fst | dot -Tpdf > L.pdf
+#fstdraw --portrait T.fst | dot -Tpdf > T.pdf
+#fstdraw --portrait TL.fst | dot -Tpdf > TL.pdf
+#fstdraw --portrait TLG.fst | dot -Tpdf > TLG.pdf
 
 set +x

b0fb745445dc1e3e5862f97b2fc6b12cd816da91
Unix&Me <unixnme@gmail.com>
2020-05-26 16:14:23 -0700

    no space transition

--- create_lex.py
+++ create_lex.py
@@ -1,7 +1,8 @@
+import numpy as np
 import argparse
 
-def Transition(istate:int, ostate:int, ilabel:str, olabel:str) -> str:
-    return ('%d\t%d\t%s\t%s\n' % (istate, ostate, ilabel, olabel))
+def Transition(istate:int, ostate:int, ilabel:str, olabel:str, weight:float=0) -> str:
+    return ('%d\t%d\t%s\t%s\t%f\n' % (istate, ostate, ilabel, olabel, weight))
 
 def ExitState(state:int) -> str:
     return ('%d\n' % state)
@@ -15,12 +16,17 @@ def main():
                             ''')
     parser.add_argument('--output_prefix', type=str, default='lexicon',
                         help='output prefix to write its txt and syms files')
+    parser.add_argument('--space_prob', type=float, default=0.5,
+            help='probability that requires space after end of a word')
     args = parser.parse_args()
 
     eps = '<epsilon>'
     space = '<space>'
     phi = '<phi>'
     unk = '<unk>'
+
+    weight_space = -np.log(args.space_prob)
+    weight_nospace = -np.log(1 - args.space_prob)
 
     vocab = open(args.vocab).read().split()
     with open(args.output_prefix + '.txt', 'w') as f:
@@ -31,9 +37,14 @@ def main():
             for i,char in enumerate(word):
                 if i == 0:
                     f.write(Transition(start, current + 1, char, word))
+                elif i == len(word) - 1:
+                    # last character can go back to start
+                    f.write(Transition(current, start, char, eps, weight_nospace))
+                    f.write(Transition(current, current + 1, char, eps, weight_space))
                 else:
                     f.write(Transition(current, current + 1, char, eps))
                 current += 1
+            # back to start w/o space
             f.write(Transition(current, start, space, eps))
         # any repetitive space shall be ignored
         f.write(Transition(start, start, space, eps))
--- hbka/run.sh
+++ hbka/run.sh
@@ -5,6 +5,7 @@ lmplz_dir=$1
 echo "be an app pa" | python ../create_lex.py
 echo "a b e n p" | python ../create_t.py
 
+echo -e "0\t0\t<phi>\t<phi>\n" >> lexicon.txt # to compose with G's backoff ilabel
 $lmplz_dir/lmplz --order 3 --discount_fallback -S 10%  < ../corpus.txt > 3gram.arpa
 ngramread --ARPA --symbols=lexicon.syms 3gram.arpa | fstarcsort --sort_type=ilabel - G.fst
 fstcompile --isymbols=tokens.syms --osymbols=lexicon.syms --keep_isymbols --keep_osymbols lexicon.txt | fstarcsort --sort_type=olabel - L.fst
@@ -12,24 +13,19 @@ fstcompile --isymbols=tokens.syms --osymbols=lexicon.syms --keep_isymbols --keep
 fstcompile --isymbols=tokens.syms --osymbols=tokens.syms --keep_isymbols --keep_osymbols tokens.txt tokens.fst
 fstcompile --isymbols=tokens.syms --osymbols=tokens.syms --keep_isymbols --keep_osymbols tokens_self.txt tokens_self.fst
 fstarcsort tokens.fst | fstphicompose 1 tokens_self.fst - | fstrmepsilon | fstdeterminize | fstminimize - | fstarcsort --sort_type=olabel - T.fst
-fstarcsort L.fst | fsttablecompose T.fst - | fstrmepsilon | fstminimizeencoded - TL.fst
 
 echo "0 1" > relabel.txt
 fstrelabel --relabel_ipairs=relabel.txt G.fst G.fst
-
-fstprint TL.fst TL.txt
-echo -e "0\t0\t<phi>\t<phi>\n" >> TL.txt # to compose with G's backoff ilabel
-fstcompile --isymbols=tokens.syms --osymbols=lexicon.syms --keep_isymbols --keep_osymbols TL.txt TL.fst
-
-fstarcsort --sort_type=olabel TL.fst | fsttablecompose - G.fst | fstdeterminizestar | fstminimizeencoded - TLG.fst
-
+fsttablecompose L.fst G.fst | fstdeterminizestar --use-log=true | fstminimizeencoded - LG.fst
 echo "1 0" > relabel.txt
-fstrelabel --relabel_ipairs=relabel.txt TLG.fst | fstrmepslocal - TLG.fst
+fstrelabel --relabel_ipairs=relabel.txt LG.fst | fstarcsort - LG.fst
+
+fsttablecompose T.fst LG.fst | fstminimizeencoded - TLG.fst
 
 fstdraw --portrait G.fst | dot -Tpdf > G.pdf
 fstdraw --portrait L.fst | dot -Tpdf > L.pdf
 fstdraw --portrait T.fst | dot -Tpdf > T.pdf
-fstdraw --portrait TL.fst | dot -Tpdf > TL.pdf
+fstdraw --portrait LG.fst | dot -Tpdf > LG.pdf
 fstdraw --portrait TLG.fst | dot -Tpdf > TLG.pdf
 
 set +x
